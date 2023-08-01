766b72f2aa01e0857ef06d8574ec00f8afc92c9d
tasm <tasm@tasm.fun>
2023-08-01 15:41:22 +0300

    fix search in inner map type of values

--- loops/secretsLoop.go
+++ loops/secretsLoop.go
@@ -6,21 +6,37 @@ import (
 	"github.com/tasm20/go-vault-search/prints"
 )
 
+func kvSecretsToMap(secretsList *api.KVSecret) map[string]interface{} {
+	res := make(map[string]interface{})
+	for keyList, value := range secretsList.Data {
+		if mapSecrets, ok := value.(map[string]interface{}); ok {
+			for keySecrets, valueSecrets := range mapSecrets {
+				res[keySecrets] = valueSecrets
+			}
+		} else {
+			res[keyList] = value
+		}
+	}
+	return res
+}
+
 func SecretsLoop(secretsList map[string]*api.KVSecret, secretsDataCh chan map[string]map[string][]byte) {
 	var secretDATA = make(map[string]map[string][]byte)
 	for key, values := range secretsList {
 		valueMapCh := make(chan map[string][]byte)
-		go innerSecretValueLoop(values, valueMapCh)
+		valuesMap := kvSecretsToMap(values)
+		go innerSecretValueLoop(valuesMap, valueMapCh)
+
 		secretDATA[key] = <-valueMapCh
 	}
 
 	secretsDataCh <- secretDATA
 }
 
-func innerSecretValueLoop(values *api.KVSecret, valueMapCh chan map[string][]byte) {
+func innerSecretValueLoop(values map[string]interface{}, valueMapCh chan map[string][]byte) {
 	var newMap = make(map[string][]byte)
 
-	for key, value := range values.Data {
+	for key, value := range values {
 		var vJson []byte
 		valueCh := make(chan interface{})
 		valuesInterface, ok := value.(map[string]interface{})
@@ -35,6 +51,25 @@ func innerSecretValueLoop(values *api.KVSecret, valueMapCh chan map[string][]byt
 
 	valueMapCh <- newMap
 }
+
+//func innerSecretValueLoop(values *api.KVSecret, valueMapCh chan map[string][]byte) {
+//	var newMap = make(map[string][]byte)
+//
+//	for key, value := range values.Data {
+//		var vJson []byte
+//		valueCh := make(chan interface{})
+//		valuesInterface, ok := value.(map[string]interface{})
+//		if ok {
+//			go innerInterfaceLoop(valuesInterface, valueCh)
+//			vJson = innerToJson(<-valueCh)
+//		} else {
+//			vJson = innerToJson(value)
+//		}
+//		newMap[key] = vJson
+//	}
+//
+//	valueMapCh <- newMap
+//}
 
 func innerInterfaceLoop(values map[string]interface{}, valueCh chan interface{}) {
 	for _, value := range values {
